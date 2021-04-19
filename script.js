a594b4874ceaf659dd63cad081e15bc4ca973795
JKGplay <74616786+JKGplay@users.noreply.github.com>
2021-04-19 18:44:30 +0200

    more updates
    zmieniający się tytuł strony, uzupełnienie tabelki o częściach Brennej i pierwsze 19 linijek kodu w JavaScripcie

---
+++ script.js
@@ -0,0 +1,20 @@
+zmianaTitle = function () {
+    function title() {
+        if (document.title === "👉Lore Brennej👈") {
+            //console.log(document.title);
+            document.title = "👇Lore Brennej👇" //&#x1F447;Lore Brennej&#x1F447;
+            //console.log(document.title);
+            return;
+        }
+        if (document.title === "👇Lore Brennej👇") {
+            //console.log(document.title);
+            document.title = "👉Lore Brennej👈" //&#x1F449;Lore Brennej&#x1F448;
+            //console.log(document.title);
+            return
+        }
+    }
+
+setInterval(title, 1500);
+}
+zmianaTitle();
+
